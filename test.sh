bd42f3687deab289fb62bd1cf594d742fbd26cec
Saragam Ghimire <codewithghimire@gmail.com>
2023-05-15 12:05:44 +0545

    started utility types

--- test.sh
+++ test.sh
@@ -2,4 +2,5 @@
 # tsc narrowing.ts && node narrowing.js
 # tsc functions.ts && node functions.js
 # tsc objectTypes.ts && node objectTypes.js
-tsc --target es2020 classes.ts && node classes.js
+# tsc --target es2020 classes.ts && node classes.js
+tsc utilityTypes.ts && node utilityTypes.js
